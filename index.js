4475b55f7994662797f82e0a88b60ddb2fc45349
Dan Park <danieljinwoopark@gmail.com>
2017-12-03 09:18:57 -0600

    config.json with bot token

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Hello world!');
+const Discord = require('discord.js')
+const bot = new Discord.Client();
+const CONFIG = require('./config.json')
+
+// bot.login(CONFIG.botToken);
+console.log(CONFIG.botToken);
