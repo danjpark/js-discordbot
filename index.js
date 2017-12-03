2ffd97746889676f0bbc6fcc31e1d2651b5e0549
Dan Park <danieljinwoopark@gmail.com>
2017-12-03 15:25:10 -0600

    added asycn ready

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const Discord = require('discord.js')
-const bot = new Discord.Client();
 const CONFIG = require('./config.json')
 
-bot.on('message', (message) => {
+// https://discord.js.org/#/docs/main/stable/typedef/ClientOptions
+const bot = new Discord.Client({disableEveryone : true});
+
+bot.on('ready', async () => {
+  console.log('Bot is ready. Username is ${bot.user.username}');
+  try {
+    let link = await bot.generateInvite(['ADMINISTRATOR']);
+    console.log(link);
+  } catch e {
+    console.log(e.stack);
+  }
+});
+
+bot.on('message', async message => {
   if(message.content == 'ping'){
     message.reply('pong');
     message.channel.sendMessage('pong');
