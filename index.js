8cee7f795b37ad3611dede514794f36642b5ac3b
Dan Park <danieljinwoopark@gmail.com>
2017-12-03 15:53:05 -0600

    basic set up

--- index.js
+++ index.js
@@ -5,21 +5,35 @@ const CONFIG = require('./config.json')
 const bot = new Discord.Client({disableEveryone : true});
 
 bot.on('ready', async () => {
-  console.log('Bot is ready. Username is ${bot.user.username}');
+  console.log(`Bot is ready. Username is ${bot.user.username}`);
   try {
     let link = await bot.generateInvite(['ADMINISTRATOR']);
     console.log(link);
-  } catch e {
+  } catch(e) {
     console.log(e.stack);
   }
 });
 
 bot.on('message', async message => {
-  if(message.content == 'ping'){
-    message.reply('pong');
-    message.channel.sendMessage('pong');
+  if(message.author.bot) return;
+  if(message.channel.type === 'dm') return;
+  if(!message.content.startsWith(CONFIG.prefix)) return;
+
+  let messageArray = message.content.split(' ');
+  let command = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if(command === `${CONFIG.prefix}userinfo`){
+    console.log(`print print print`);
   }
+
 });
 
 bot.login(CONFIG.botToken);
 // console.log(CONFIG.botToken);
+
+function getCyrptoPrice() {
+
+
+  return
+}
