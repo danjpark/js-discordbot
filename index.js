0b38d17e1aa237994773c1fa45831fded6440c61
Dan Park <danieljinwoopark@gmail.com>
2017-12-04 15:46:16 -0600

    rich embed with different exchanges

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ bot.on('ready', async () => {
 bot.on('message', async message => {
   if(message.author.bot) return;
   if(message.channel.type === 'dm') return;
+  if(message.channel.name != 'crypto') return;
   if(!message.content.startsWith(CONFIG.prefix)) return;
 
   let messageArray = message.content.split(' ');
@@ -28,45 +29,37 @@ bot.on('message', async message => {
 
   if(command === `${CONFIG.prefix}crypto`){
 
-    const topExchanges = _.map(await cc.topExchanges(args[0], 'USD', 4),
-                         item => {return item.exchange;});
+    try {
+      const topExchanges = _.map(await cc.topExchanges(args[0], 'USD', (parseInt(args[1])||3)),
+                           item => {return item.exchange;});
 
-    let promArray = [];
-    _.each(topExchanges, exchange => {
-      promArray.push(cc.price(args[0],'USD',{exchanges:exchange}));
-    });
+      let promArray = [];
+      _.each(topExchanges, exchange => {
+        promArray.push(cc.price(args[0],'USD',{exchanges:exchange}));
+      });
 
-    const topPrices = await Promise.all(promArray);
+      const topPrices = await Promise.all(promArray);
 
-    const cleanResult = _.map(_.zip(topExchanges, topPrices), exchPrice => {
-      // return(exchPrice[0] + " : " + exchPrice[1].USD.toFixed(2).toString());
-      return {exchange: exchPrice[0], price: exchPrice[1].USD};
-    });
+      const cleanResult = _.map(_.zip(topExchanges, topPrices), exchPrice => {
+        return {exchange: exchPrice[0], price: exchPrice[1].USD};
+      });
 
+      let retString = _.reduce(cleanResult, function(memo, obj) {
+        return memo + obj.exchange + ': `' + obj.price + '`\n';
+      }, '');
 
-    //_.zip(topExchanges, topPrices)
+      let richData = new Discord.RichEmbed()
+        .setTitle(args[0])
+        .setDescription(retString)
+        .setTimestamp();
 
-    // [ [ 'Coinbase', { USD: 11395 } ],
-    // [ 'Bitfinex', { USD: 11280 } ],
-    // [ 'Bitstamp', { USD: 11337.36 } ],
-    // [ 'Gemini', { USD: 11402.08 } ] ]
-
-
-    
-    // let richData = new Discord.RichEmbed()
-    //   .setTitle(args[0])
-    //   .setDescription(exchanges[0].exchange)
-    //   .setTimestamp();
-    //
-    // message.channel.sendEmbed(richData);
+      message.channel.send(richData);
+    } catch(e) {
+      message.channel.send(e);
+    }
 
 
   }
 });
 
 bot.login(CONFIG.botToken);
-
-// TO DO:
-// figure out how to explicitally call exchange in cc.prices
-// use underscore to do it for each in list
-// figure out how to pass in default param (if nothing just pass in 2 for test and then 1)
