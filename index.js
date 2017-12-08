1b9d593eb4e4bf3dd3c89885f81465fb469e604c
Dan Park <danieljinwoopark@gmail.com>
2017-12-07 18:06:30 -0600

    adding yahoo file

--- index.js
+++ index.js
@@ -6,58 +6,59 @@ const CONFIG = require('./config.json');
 const bot = new Discord.Client({disableEveryone : true});
 
 bot.on('ready', async () => {
-  console.log(`Bot is ready. Username is ${bot.user.username}`);
-  try {
-    let link = await bot.generateInvite(['ADMINISTRATOR']);
-    console.log(link);
-  } catch(e) {
-    console.error(e);
-  }
+    console.log(`Bot is ready. Username is ${bot.user.username}`);
+    try {
+        let link = await bot.generateInvite(['ADMINISTRATOR']);
+        console.log(link);
+    } catch(e) {
+        console.error(e);
+    }
 });
 
 bot.on('message', async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === 'dm') return;
-  if(message.channel.name != 'crypto') return;
-  if(!message.content.startsWith(CONFIG.prefix)) return;
+    if(message.author.bot) return;
+    if(message.channel.type === 'dm') return;
+    if(message.channel.name != 'crypto') return;
+    if(!message.content.startsWith(CONFIG.prefix)) return;
 
-  const [
-    command,
-    coin = 'BTC',
-    target = 'USD',
-    numExchanges = 3
-  ] = message.content.split(' ');
+    const [
+        command,
+        symbol = 'BTC',
+        target = 'USD',
+        numExchanges = 3
+    ] = message.content.split(' ')
+        .map((eachString => {return eachString.toUpperCase()}));
 
-  console.log(command, coin, target, numExchanges);
+    console.log(command, symbol, target, numExchanges);
 
-  if(command === `${CONFIG.prefix}crypto`){
+    if(command === `${CONFIG.prefix}CRYPTO`){
 
-    try {
-      const topExchanges = (await cc.topExchanges(coin, target, numExchanges))
+        try {
+            const topExchanges = (await cc.topExchanges(symbol, target, numExchanges))
                 .map(item => item.exchange);
 
-      const promises = topExchanges.map(async exchange => {
-        const price = await cc.price(coin, target, {exchanges: exchange});
-        return `${exchange}: ${price[target]}`;
-      })
+            const promises = topExchanges.map(async exchange => {
+                const price = await cc.price(symbol, target, {exchanges: exchange});
+                return `${exchange}: ${price[target]}`;
+            })
 
-      const result = (await Promise.all(promises)).join('\n')
+            const result = (await Promise.all(promises)).join('\n')
 
-      console.log(result);
-      let richData = new Discord.RichEmbed()
-        .setTitle(coin)
-        .setDescription(result)
-        .setTimestamp();
+            console.log(result);
+            let richData = new Discord.RichEmbed()
+                .setTitle(symbol)
+                .setDescription(result)
+                .setTimestamp();
 
-      message.channel.send(richData);
-    } catch(e) {
-      message.channel.send(e);
+            message.channel.send(richData);
+        } catch(e) {
+            message.channel.send(e);
+        }
+
+
+    } else if (command === `${CONFIG.prefix}STOCK`){
+        message.channel.send('stock!');
     }
-
-
-  } else if (command === `${CONFIG.prefix}stock`){
-    message.channel.send('stock!');
-  }
 });
 
 bot.login(CONFIG.botToken);
